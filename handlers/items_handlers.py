1eb3aa9341369bc0c0d295d2dbb6ec6d5076e4f8
tho deb <gentimouton@gmail.com>
2015-12-28 19:12:29 -0800

    added item db file, and display level and name for items

--- handlers/items_handlers.py
+++ handlers/items_handlers.py
@@ -1,9 +1,23 @@
+import logging
 from handlers.base import BaseHandler
+from db.items_db import item_db  # @UnresolvedImport
 
 
 class ItemCategoryHandler(BaseHandler):
     def get(self, **kwargs):
-        context = {'category': kwargs['category']}
+        category = kwargs['category']
+        items = item_db[category.capitalize()]
+        ordered_items = [{
+            'name': name, 
+            'level': data['level'], 
+            'price': data['price'],
+            'img': '/static/%s/%s.png' % (category, name.replace(' ', '_'))
+            } for name, data in items.items()]
+        ordered_items.sort(key=lambda item: item['level'])
+        logging.info(str(ordered_items))
+        context = {'category': category,
+            'items': ordered_items
+            }
         self.render_response('category.html', **context)
 
 class ItemListHandler(BaseHandler):
