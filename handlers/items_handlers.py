87977b707a0eaf9653442019222a87b13656d9d5
tho deb <gentimouton@gmail.com>
2015-12-30 23:41:54 -0800

    added missing scrolls, display resource costs

--- handlers/items_handlers.py
+++ handlers/items_handlers.py
@@ -2,21 +2,39 @@ import logging
 from handlers.base import BaseHandler
 from db.items_db import item_db  # @UnresolvedImport
 
+# ordered list of mats_list
+mats_list = ['iron', 'wood', 'leather', 'herbs',
+             'steel', 'hwood', 'fabric', 'oil', 'mana', 'jewels']
 
 class ItemCategoryHandler(BaseHandler):
     def get(self, **kwargs):
         category = kwargs['category']
-        items = item_db[category.capitalize()]
-        ordered_items = [{
-            'name': name, 
-            'level': data['level'], 
-            'price': data['price'],
-            'img': '/static/%s/%s.png' % (category, name.replace(' ', '_'))
-            } for name, data in items.items()]
-        ordered_items.sort(key=lambda item: item['level'])
-        logging.info(str(ordered_items))
+        items = []
+        for item_name, item_data in item_db[category.capitalize()].items():
+            # materials listing
+            mats_display = []
+            mats_required = item_data['resources'].keys()
+            for m in mats_list:
+                if m in mats_required:
+                    qty = item_data['resources'][m]
+                    mat_display = {
+                        'qty': qty,
+                        'img': '/static/resources/%s.png' % m
+                        }
+                    mats_display.append(mat_display)
+            # item level, img, and price
+            img = '/static/%s/%s.png' % (category, item_name.replace(' ', '_'))
+            item = {
+                'name': item_name,
+                'level': item_data['level'], 
+                'price': item_data['price'],
+                'img': img,
+                'mats': mats_display
+            }
+            items.append(item)
+        items.sort(key=lambda item: item['level'])
         context = {'category': category,
-            'items': ordered_items
+            'items': items
             }
         self.render_response('category.html', **context)
 
