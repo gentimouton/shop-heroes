30241de9bec8453eda3e25e218626747672ad943
tho deb <gentimouton@gmail.com>
2016-01-29 18:09:10 -0800

    created page for search results

--- handlers/misc_handlers.py
+++ handlers/misc_handlers.py
@@ -16,3 +16,9 @@ class HomeHandler(BaseHandler):
     def get(self):
         context = {}
         self.render_response('home.html', **context)
+
+class SearchHandler(BaseHandler):
+    def get(self):
+        query = self.request.get('q')
+        context = {'query': query}
+        self.render_response('search.html', **context)
--- routes.py
+++ routes.py
@@ -2,11 +2,12 @@ import webapp2
 
 from handlers.items_handlers import ItemHandler, ItemCategoryHandler
 from handlers.misc_handlers import HomeHandler, HeroListHandler, \
-    FurnitureListHandler
+    FurnitureListHandler, SearchHandler
 
 
 # See http://webapp-improved.appspot.com/guide/routing.html#simple-routes
 routes = [webapp2.Route(r'/', handler=HomeHandler, name='home'),
+    webapp2.Route(r'/search', handler=SearchHandler, name='search'),
     webapp2.Route(r'/items/<category:\D+>', handler=ItemCategoryHandler, name='category'),
     webapp2.Route(r'/item/<slug:\D+>', handler=ItemHandler, name='item'),
     webapp2.Route(r'/heroes', handler=HeroListHandler, name='item-list'),
