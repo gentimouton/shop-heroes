d18ac0bdbbda75f53346ce821f174fab4a2ecd5f
tho deb <gentimouton@gmail.com>
2016-01-31 20:37:10 -0800

    display skill requirements for each item

--- handlers/items_handlers.py
+++ handlers/items_handlers.py
@@ -5,7 +5,7 @@ from db.db_resources import resource_db
 from db.db_items import item_db
 from db.db_categories import categories_db
 from handlers.base import BaseHandler
-
+from db.db_skills import skill_db
 
 def format_qty(qty):
     """
@@ -35,13 +35,17 @@ resource_slugs = sorted(resource_db.keys(),
     key=lambda slug: resource_db[slug]['rank'])
 artifact_slugs = sorted(artifact_db.keys(),
     key=lambda slug: artifact_db[slug]['level'])
+skill_slugs = sorted(skill_db.keys())
+
 
 # map resource and artifact slugs to their position in the icons sprite
 icons_map = [resource_slugs,
     artifact_slugs[0:10],
     artifact_slugs[10:20],
     artifact_slugs[20:len(artifact_slugs)],
-    ['coin', 'gem', 'time', 'barracks', 'power', 'heart']
+    ['coin', 'gem', 'time', 'barracks', 'power', 'heart'],
+    skill_slugs[0:10],
+    skill_slugs[10:len(skill_slugs)]
     ]
 
 
@@ -86,6 +90,15 @@ def get_item_data(item_slug):
                 'quality': comp_data[1]}
             mats_display.append(mat)
                 
+    # build list of skills
+    skills_display = [] # [{'slug': 'tinkering', 'qty': 5, 'name': 'Tinkering'}]
+    required_skills = item_data['skills'] 
+    for skill_slug in skill_slugs:
+        if skill_slug in required_skills.keys():
+            skill = {'slug': skill_slug,
+                'name': skill_db[skill_slug],
+                'qty': str(format_qty(required_skills[skill_slug]))}
+            skills_display.append(skill)
     
     # add item name, level, img, and price
     item = {
@@ -95,7 +108,8 @@ def get_item_data(item_slug):
         'level': item_data['level'],
         'price': format_qty(item_data['price']),
         'power': item_data['power'],
-        'mats': mats_display
+        'mats': mats_display,
+        'skills': skills_display
     }
     return item
 
